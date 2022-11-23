64af8199bb5df63b275dfbaad6b6a85be1e1d0c4
Galina <gal-rassohina@yandex.ru>
2022-11-23 22:52:44 +0700

    программа

--- Desktop/резюме/Учеба Аналитика/Seminar3/Program.cs
+++ Desktop/резюме/Учеба Аналитика/Seminar3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4 8 -> 2, 4, 6, 8
+int N = 5;
+{
+int k = 2;
+if (k <= N)
+Console.Write(k);
+k++;
+}
